e80d239bcf6eb1cf1f78e94d70f5f6c41215930a
Giovanni <g.marino30@studenti.uniba.it>
2018-01-06 16:21:08 +0100

    security

--- servlet/LoginServlet.java
+++ servlet/LoginServlet.java
@@ -123,8 +123,8 @@ public class LoginServlet extends HttpServlet {
 			
 			HttpSession session = request.getSession();
 			
-			//MyUtils.storeLoginedUser(session, name, chiave);
-			MyUtils.storeLoginedUser(session, user);
+			MyUtils.storeLoginedUser(session, name, chiave);
+			//MyUtils.storeLoginedUser(session, user);
 			
 			//if user checked "Remenber Me"
 			rememberMe(user, remember, response);
